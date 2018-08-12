f1420c1a2f37deb7fa31d8cd6ab14e66ff3881aa
Dick Shern <eowdickshern@gmail.com>
2018-08-12 21:20:09 +0800

    Optimize process by reducing run frequency during off-peak period

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import processing
 import time
-from myconfigurations import ITEMS, FREQUENCY
+from myconfigurations import ITEMS, FREQUENCY, FREQUENCY_SLOW, DELAY_START, DELAY_STOP
 import slackclient as robot
+import schedule
+import chatbot_slack as robot
+
+def job_delay_alter(inFrequency):
+    global frequency
+    frequency = inFrequency
+    robot.post_message("JOB UPDATED")
 
 if __name__ == "__main__":
     starttime = time.time()
+    frequency = FREQUENCY
+    schedule.every().day.at(DELAY_START).do(job_delay_alter, FREQUENCY_SLOW)
+    schedule.every().day.at(DELAY_STOP).do(job_delay_alter, FREQUENCY)
 
     while True:
+        schedule.run_pending()
         try:
             for idx, i in enumerate(ITEMS):
                 try:
@@ -17,6 +28,4 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             print ("Interrupt")
             sys.exit(1)
-        # time.sleep(600)
-        time.sleep(FREQUENCY - ((time.time() - starttime) % FREQUENCY))
-
+        time.sleep(frequency - ((time.time() - starttime) % frequency))
